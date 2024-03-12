d71789eb3210c4bc6cc670314754bf6d34c2709a
Alphonse <prudent.nkubito@gmail.com>
2024-03-12 15:23:43 -0700

    task 3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for y in range (0, 10):
+    for z in range(y+1, 10):
+        if y == 9 and z == 5:
+            print('95')
+        else:
+            print('{}{}, '.format(y, z), end= ""')
