572960ee37166712ce437289f32defe35aa67822
Alphonse <prudent.nkubito@gmail.com>
2024-03-17 06:13:40 -0700

    function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,34 +3,40 @@
 #include <stdio.h>
 
 /**
- * is_palindrome - It checks a singly linked list if is a palindrome
- * @head: point to linked list
- * Return: checks, 1 if is palindrome, 0 if not
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: Pointer to the head of the linked list
+ * Return: 1 if palindrome, 0 if not
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *fast, *renew, *eye1 = NULL, *eye2 = NULL;
+    listint_t *fast, *reverse, *temp1 = NULL, *temp2 = NULL;
 
-	if(!*head || !(*head)->next)
-		return (1);
-	fast = *head;
-	renew = *head;
-	while (renew->next)
-		renew = renew->next;
-	while (renew)
-	{
-		eye1 = renew->next;
-		renew->next = eye2;
-		eye2 = renew;
-		renew = eye1;
-	}
-	renew = eye2;
-	while (fast && renew)
-	{
-		if (fast->n != renew->n)
-			return (0);
-		fast = fast->next;
-		renew = renew->next;
-	}
-	return (1);
+ 	if (!*head || !(*head)->next)
+        	return (1);
+
+    fast = *head;
+    reverse = *head;
+
+    while (reverse->next)
+        reverse = reverse->next;
+
+    while (reverse)
+    {
+        temp1 = reverse->next;
+        reverse->next = temp2;
+        temp2 = reverse;
+        reverse = temp1;
+    }
+
+    reverse = temp2;
+
+	 while (fast && reverse)
+    	{
+        	if (fast->n != reverse->n)
+            		return (0);
+        fast = fast->next;
+        reverse = reverse->next;
+    }
+
+    return (1);
 }
