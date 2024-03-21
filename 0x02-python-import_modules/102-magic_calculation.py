00d2816b660e03ab48efe403acc6a59dda09c50d
Alphonse <prudent.nkubito@gmail.com>
2024-03-20 17:00:01 -0700

    Python function that print bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,8 +5,9 @@ def magic_calculation(a, b):
     """Match Bytecode of Holberton"""
     if a < b:
         c = add(a, b)
+
         for i in range(4, 6):
             c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+            return (c)
+        else:
+            return sub(a, b)
