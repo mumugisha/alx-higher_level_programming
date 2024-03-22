23b89cc941b547a07ef6bd7505fe8b75701c257b
Alphonse <prudent.nkubito@gmail.com>
2024-03-22 16:52:43 -0700

    function that convert to an integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
+    total = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+    for i in range(len(roman_string)):
+        if i > 0 and digits[roman_string[i]] > digits[roman_string[i - 1]]:
+            total += digits[roman_string[i]] - 2 * digits[roman_string[i - 1]]
+        else:
+            total += digits[roman_string[i]]
+
+    return total
