4bd56a65d4a878439adc9f2221cef5b5b4cc0f86
Alphonse <prudent.nkubito@gmail.com>
2024-04-02 17:25:14 -0700

    Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
+
 def safe_function(fct, *args):
     try:
         sand = fct(*args)
         return sand
-    except Exception as err:
-        print("Exception: {}".format(err), file=sys.stderr)
-   return None
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+    return None
