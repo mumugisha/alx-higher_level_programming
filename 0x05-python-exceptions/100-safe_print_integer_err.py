3df8cef702e30fa1d49a783c9d596b34b12b65ea
Alphonse <prudent.nkubito@gmail.com>
2024-04-02 17:03:02 -0700

    Exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as er:
-        print("Exception: {}".format(er), file=sys.stderr)
+    except (ValueError, TypeError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
         return False
