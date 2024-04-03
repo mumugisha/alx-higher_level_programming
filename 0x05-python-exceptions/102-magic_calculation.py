3e2d29a5ce86d2a971eaf7fe0728dff9d74ef64c
Alphonse <prudent.nkubito@gmail.com>
2024-04-03 10:54:55 -0700

    Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 
-import sys
-
 def magic_calculation(a, b):
     sand = 0
     try:
-        if a < 1:
-            raise ValueError('a must be at least 1')
-        else:
-            for u in range(1, 2):
+        for u in range(1, 3):
+            if u > 1:
+                raise Exception('big up')
+            else:
                 sand += (a ** b) / u
-    except ValueError:
+    except Exception:
         sand = b + a
+    
     return sand
-
-if __name__ == "__main__":
-    try:
-        a = int(sys.argv[1])
-        b = int(sys.argv[2])
-        print(magic_calculation(a, b))
-    except IndexError:
-        print("Usage: python3 script.py <a> <b>")
-    except ValueError:
-        print("Both 'a' and 'b' must be integers.")
