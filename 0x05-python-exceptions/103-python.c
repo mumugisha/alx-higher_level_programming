7ef2c7d0ab8337b6a02aec8ea570532c201b04fa
Alphonse <prudent.nkubito@gmail.com>
2024-04-03 23:32:35 -0700

    print projects 5

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,44 +1,56 @@
 #include <Python.h>
 
 /**
- * File: print PyFloatObject
+ * File: PyFloatObject
  * Author: By Mugisha Alphonse
  */
 
+// Function prototypes
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
 
 /**
- * print_python_list - Print info of basics in Python list
- * @p: pointer to PyObject list
+ * print_python_list - Print info about Python list
+ * @p: Pointer to PyObject list
  */
 void print_python_list(PyObject *p)
 {
+    // Variables to store size and allocation of list
     Py_ssize_t size, alloc, u;
     const char *type;
+    
+    // Cast the PyObject to PyListObject
     PyListObject *list = (PyListObject *)p;
+    // Cast the PyObject to PyVarObject
     PyVarObject *var = (PyVarObject *)p;
 
+    // Extract size and allocation
     size = var->ob_size;
     alloc = list->allocated;
 
+    // Flush standard output
     fflush(stdout);
 
+    // Print Python list info
     printf("[*] Python list info\n");
-    if (strcmp(p->ob_type->tp_name, "list") != 0)
+    if (!PyList_Check(p))
     {
         printf("  [ERROR] Invalid List Object\n");
         return;
     }
 
+    // Print size and allocation information
     printf("[*] Size of the Python List = %ld\n", size);
     printf("[*] Allocated = %ld\n", alloc);
 
+    // Loop through elements of the list
     for (u = 0; u < size; u++)
     {
-        type = list->ob_item[u]->ob_type->tp_name;
+        // Get the type of the current element
+        type = Py_TYPE(list->ob_item[u])->tp_name;
         printf("Element %ld: %s\n", u, type);
+        // Call appropriate function based on the type of element
         if (strcmp(type, "bytes") == 0)
             print_python_bytes(list->ob_item[u]);
         else if (strcmp(type, "float") == 0)
