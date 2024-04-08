1220b01b1315b72f9a8bd82760fa853540f23574
Alphonse <prudent.nkubito@gmail.com>
2024-04-08 13:26:40 -0700

    more classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""A group to represent rectangle class."""
+
+class Rectangle:
+    """I represent a class of rectangle."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+This is a new project about classes
+0x08. Python - More Classes and Objects
