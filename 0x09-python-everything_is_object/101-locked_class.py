512694ce53487b4e48980ead155b380675d73385
Alphonse <prudent.nkubito@gmail.com>
2024-04-10 03:25:34 -0700

    everything is object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""" this is class locked attributes """"
+""" this is class locked attributes """
+
 
 class LockedClass:
+    """ do not initiate another lockedClass, but only 'first_name'.
+    """
     __slot__ = ["first_name"]
