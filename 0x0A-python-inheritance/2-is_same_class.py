723c6e275993d556d61cbabdff6a50d32b5cff1d
Alphonse <prudent.nkubito@gmail.com>
2024-04-15 13:06:08 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define class that checks other function."""
+
+
+def is_same_class(obj, v_class):
+    """Check if object is exactly an instance of certain class.
+    Args:
+        obj: object that need checking.
+        v_class: A class to match a type of obj to define.
+    Returns:
+        If obj is exactly an instance of v_class - True.
+        Otherwise - False.
+    """
+    if type(obj) == v_class:
+        return True
+    return False
