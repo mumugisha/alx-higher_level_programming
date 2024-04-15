d1d22bb50759505a709511fcd8cb94b1a85c647a
Alphonse <prudent.nkubito@gmail.com>
2024-04-15 14:22:33 -0700

    python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,5 +13,3 @@ def inherits_from(obj, a_class):
         bool: True if obj inherits from a_class, otherwise False.
     """
     return isinstance(obj, a_class) and type(obj) != a_class
-	return True
-    return False
