4a886c7b4b1e2ab4746d55429f5659455d66469b
Alphonse <prudent.nkubito@gmail.com>
2024-04-16 12:16:17 -0700

    input and output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Define a class Student by specialization."""
+
+
+class Student:
+    """Represent a class of student."""
+
+    def __init__(self, first_name, last_name, age):
+        """I initialize a new Student.
+        Args:
+            first_name (str): The first name of the student class.
+            last_name (str): The last name of the student class.
+            age (int): The age of the student class.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of a class of student."""
+        return self.
