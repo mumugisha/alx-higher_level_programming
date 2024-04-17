9c9622101fe41bf1e86b3fce073ee065d5c5c599
Alphonse <prudent.nkubito@gmail.com>
2024-04-17 12:44:51 -0700

    everything is object

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list and save them to a file."""
+""" Define argument to a Python list and save them to a file."""
+
 
 import sys
 
