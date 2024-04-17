8b27b727810618a9631e32800987ab2ffa5caab6
Alphonse <prudent.nkubito@gmail.com>
2024-04-17 12:10:05 -0700

    everything is object

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,13 +1,17 @@
+0x0B-python-input_output/101-stats.py
+
 #!/usr/bin/python3
 
-"""Reads standard input line by line and computes metrics:
-    - Total file size.
-    - Number of lines by status code.
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
 
 
 def print_stats(size, status_codes):
-    """Print line by line and computes metrics.
+    """Print accumulated metrics.
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
