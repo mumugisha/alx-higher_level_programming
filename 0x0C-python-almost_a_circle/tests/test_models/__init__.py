44de0eeea8ed36c61472fdbc13ad86e198bc0d59
Ankubito <prudent.nkubito@gmail.com>
2024-04-22 15:20:13 -0700

    python almost cycle

--- 0x0C-python-almost_a_circle/tests/test_models/__init__.py
+++ 0x0C-python-almost_a_circle/tests/test_models/__init__.py
@@ -1,8 +1,70 @@
+#!/usr/bin/python3
+"""Unittest classes:
+    TestBase_instantiation - line 23
+    TestBase_to_json_string - line 110
+    TestBase_save_to_file - line 156
+    TestBase_from_json_string - line 234
+    TestBase_create - line 288
+    TestBase_load_from_file - line 340
+    TestBase_save_to_file_csv - line 406
+    TestBase_load_from_file_csv - line 484
+"""
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
+class TestBase_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Base class."""
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    # Other tests for Base instantiation...
+
+    def test_two_args(self):
+        with self.assertRaises(TypeError):
+            Base(1, 2)
 
 
+class TestBase_to_json_string(unittest.TestCase):
+    """Unittests for testing to_json_string method of Base class."""
 
+    # Test cases for Base.to_json_string...
 
+class TestBase_save_to_file(unittest.TestCase):
+    """Unittests for testing save_to_file method of Base class."""
 
+    # Test cases for Base.save_to_file...
 
+class TestBase_from_json_string(unittest.TestCase):
+    """Unittests for testing from_json_string method of Base class."""
+
+    # Test cases for Base.from_json_string...
+
+class TestBase_create(unittest.TestCase):
+    """Unittests for testing create method of Base class."""
+
+    # Test cases for Base.create...
+
+class TestBase_load_from_file(unittest.TestCase):
+    """Unittests for testing load_from_file_method of Base class."""
+
+    # Test cases for Base.load_from_file...
+
+class TestBase_save_to_file_csv(unittest.TestCase):
+    """Unittests for testing save_to_file_csv method of Base class."""
+
+    # Test cases for Base.save_to_file_csv...
+
+class TestBase_load_from_file_csv(unittest.TestCase):
+    """Unittests for testing load_from_file_csv method of Base class."""
+
+    # Test cases for Base.load_from_file_csv...
+
+if __name__ == "__main__":
+    unittest.main()
