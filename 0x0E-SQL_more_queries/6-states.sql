176a08a6216d82bd5b557a8c9c8fefffedf653f5
Ankubito <prudent.nkubito@gmail.com>
2024-05-22 17:10:18 -0700

    More Queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,11 @@
--- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- Script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NULL PRIMARY KEY AUTO_INCREMENT, name (VARCHAR(256) NOT NULL);
+
+-- Selecting the newly created database
+USE hbtn_0d_usa;
+
+-- Creating the table states
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
