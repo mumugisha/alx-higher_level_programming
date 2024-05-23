fc07b40f2051b3786df0eaf4c603b650d00aacf9
Ankubito <prudent.nkubito@gmail.com>
2024-05-22 18:13:08 -0700

    More Queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,4 @@
--- Lists all privileges of the MySQL user `user_0d_1` on your server (in localhost).
+-- lists all privileges of the MySQL users user_0d_1 
+-- and user_0d_2 on your server (in localhost).
 SHOW GRANTS FOR `user_0d_1`@`localhost`;
-
--- Lists all privileges of the MySQL user `user_0d_2` on your server (in localhost).
 SHOW GRANTS FOR `user_0d_2`@`localhost`;
-
--- Refresh the privileges to ensure that any changes made manually are applied.
-FLUSH PRIVILEGES;
