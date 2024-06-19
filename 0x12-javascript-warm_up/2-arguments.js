9edf22088c22de23a2987d71fcfbbdca08ba6c44
Ankubito <prudent.nkubito@gmail.com>
2024-06-19 09:45:21 -0700

    javascript-warm_up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,5 +7,5 @@ if (argv.length === 2) {
 } else if (argv.length === 3) {
     console.log('Argument found');
 } else if (argv.length > 3) {
-    console.log('Arguments found');
+	  console.log('Arguments found');
 }
