6ccbb7ee8dec8608baf234d678ed149e51ec0f6b
Ankubito <prudent.nkubito@gmail.com>
2024-06-19 14:30:19 -0700

    javascript-warm_up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,10 @@
 const { argv } = require('process');
 
 function factorial (number) {
-  if (number <= 1) return 1;
+  if (number <= 1) return (1);
   
-  return factorial(number - 1) * number;
+  return (factorial(number - 1) * number);
 }
 
 const recursev = parseInt(argv[2]);
-console.log(isNaN(recursev) ? 1 : factorial(recursev));
+console.log(Number.isNaN(recursev) ? 1 : factorial(recursev));
