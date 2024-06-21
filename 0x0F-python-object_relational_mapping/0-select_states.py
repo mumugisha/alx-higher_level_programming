67dfcaa677ae0ded939ed20115db8d9075384835
Ankubito <prudent.nkubito@gmail.com>
2024-06-21 13:57:03 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,20 +8,21 @@ if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-    
+
     connec = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=mysql_username,
         passwd=mysql_password,
-        db=database_name
+        db=database_name,
+        unix_socket="/var/run/mysqld/mysqld.sock"
     )
-    
+
     curs = connec.cursor()
     curs.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = curs.fetchall()
     for row in rows:
         print(row)
-    
+
     curs.close()
     connec.close()
