601cf7e0c84c4c65de213e80e84fdddf65d33532
Ankubito <prudent.nkubito@gmail.com>
2024-06-21 13:54:21 -0700

    Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    
+    connec = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+    
+    curs = connec.cursor()
+    curs.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    
+    curs.close()
+    connec.close()
