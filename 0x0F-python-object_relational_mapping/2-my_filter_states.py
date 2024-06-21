6a3a66d5d2a06c053351d4af3bc6022ae920de66
Ankubito <prudent.nkubito@gmail.com>
2024-06-21 14:58:50 -0700

    javascript-warm_up

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,11 +19,9 @@ if __name__ == "__main__":
             passwd=mysql_password,
             db=database_name
         )
-
         curs = connec.cursor()
         query = ("SELECT * FROM states WHERE name=%s ORDER BY states.id ASC")
         curs.execute(query, (state_name,))
-
         rows = curs.fetchall()
         for row in rows:
             if row[1] == state_name:
