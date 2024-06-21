78f7eef2e594e05285749b9c300fa9a9da019d6b
Ankubito <prudent.nkubito@gmail.com>
2024-06-21 14:12:31 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N (upper N) 
+"""Script that lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa."""
 
 import sys
@@ -19,9 +19,11 @@ if __name__ == "__main__":
     )
 
     curs = connec.cursor()
-    query = ("SELECT * FROM states "
-             "WHERE name LIKE BINARY 'N%' "
-             "ORDER BY states.id ASC")
+    query = (
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY 'N%' "
+        "ORDER BY states.id ASC"
+    )
     curs.execute(query)
     
     rows = curs.fetchall()
