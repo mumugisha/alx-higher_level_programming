9890acd03dd9c2fa86450c6500639c85bffbde51
Ankubito <prudent.nkubito@gmail.com>
2024-06-22 16:07:25 -0700

    javascript-warm_up

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
 """
-Python file that contains the class definition of a State and an instance Base.
+Contains the class definition of a State
 """
+
 from sqlalchemy import Column, Integer, String
-from sqlalchemy(create_engine)
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    Representation of the table states
+    """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-def main():
-    engine = create_engine('sqlite:///states.db', echo=True)
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-
-if __name__ == "__main__":
-    main()
