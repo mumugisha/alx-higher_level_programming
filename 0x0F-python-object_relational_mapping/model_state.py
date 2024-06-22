3e1a577a176af85c9a3e1b3f653d6232afc2c183
Ankubito <prudent.nkubito@gmail.com>
2024-06-22 14:03:33 -0700

    javascript-warm_up

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python file that contains the class definition of a State and an instance Base.
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
