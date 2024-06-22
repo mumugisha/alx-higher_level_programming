54bff673be47cfecdba695dd09470e006c8d3fb7
Ankubito <prudent.nkubito@gmail.com>
2024-06-22 16:22:45 -0700

    javascript-warm_up

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a State
+Python file that contains the class definition of a State and establishes
+a connection to a MySQL server.
 """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 
 class State(Base):
     """
-    Representation of the table states
+    Represents a State with an id and a name column.
+
+    Attributes:
+        id (int): The state's unique identifier, auto-generated, unique,
+                  non-null, and primary key.
+        name (str): The state's name, a string with a maximum length of
+                    128 characters and cannot be null.
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+DATABASE_URI = 'mysql+mysqldb://username:password@localhost:3306/database_name'
+engine = create_engine(DATABASE_URI)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
