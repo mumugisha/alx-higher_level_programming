8c681255581b01d6e34bf19feb67c1be32240f9f
Ankubito <prudent.nkubito@gmail.com>
2024-06-22 18:59:45 -0700

    javascript-warm_up

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""Script that adds the State object “Louisiana” to
+"""
+Script that adds the State object “Louisiana” to
 the database hbtn_0e_6_usa
 """
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: {} <mysql_username> <mysql_password> "
-              "<database_name> <state_name>".format(sys.argv[0]))
-        sys.exit(1)
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-    state_name = sys.argv[4]
+
 
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
@@ -28,7 +24,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    create_new_state = State(name=state_name)
+    create_new_state = State(name="Louisiana")
     session.add(create_new_state)
     session.commit()
 
