7bbddaa02570712cfa71a2a71a00c5636ee35e4b
Ankubito <prudent.nkubito@gmail.com>
2024-06-22 17:27:43 -0700

    javascript-warm_up

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,6 +8,7 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <mysql_username> <mysql_password> <database_name>"
