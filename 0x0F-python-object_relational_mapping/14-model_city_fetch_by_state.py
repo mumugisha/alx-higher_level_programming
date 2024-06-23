2ae76ef5f259a2d22d6b023e531a31ed87b0709d
Ankubito <prudent.nkubito@gmail.com>
2024-06-23 14:51:20 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,23 +7,30 @@ from model_city import City
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>"
-              .format(sys.argv[0]))
+        print(
+            "Usage: {} <username> <password> <database>".format(
+                sys.argv[0]
+            )
+        )
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, database),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database
+        ),
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     cities_query = session.query(City, State.name).join(
-        State, State.id == City.state_id).order_by(City.id).all()
+        State, State.id == City.state_id
+    ).order_by(City.id).all()
 
     for city, state_name in cities_query:
         print("{}: ({}) {}".format(state_name, city.id, city.name))
