7af37e88688b5cb7823ffde93f224a99cc78f6cc
Ankubito <prudent.nkubito@gmail.com>
2024-06-23 07:19:53 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,40 +4,35 @@
 import sys
 import MySQLdb
 
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: python 4-cities_by_state.py <mysql_username> "
-              "<mysql_password> <database_name>")
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
     try:
-        db = MySQLdb.connect(
-            host="localhost",
+        connec = MySQLdb.connect(
+            host='localhost',
             port=3306,
-            user=username,
-            passwd=password,
-            db=database
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
         )
-        cursor = db.cursor()
-        cursor.execute(
+        curs = connec.cursor()
+        query = (
             "SELECT cities.id, cities.name, states.name "
             "FROM cities "
             "INNER JOIN states ON cities.state_id = states.id "
             "ORDER BY cities.id ASC;"
         )
 
-        rows = cursor.fetchall()
+        curs.execute(query)
+        rows = curs.fetchall()
         for row in rows:
-            print(f"{row[1]}, {row[2]}")
+            print(row)
 
-        cursor.close()
-        db.close()
+        curs.close()
+        connec.close()
     except MySQLdb.Error as e:
-        print(f"Error: {e}")
-
-
-if __name__ == "__main__":
-    main()
+        print(f"Error {e}")
+    except IndexError:
+        print("Usage: ./script.py <mysql_username> <mysql_password> <database_name>")
