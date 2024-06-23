b03e415fbb8e9ac009f62280a0ac27a32c416b96
Ankubito <prudent.nkubito@gmail.com>
2024-06-23 13:21:16 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -9,6 +9,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
