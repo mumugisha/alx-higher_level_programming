22360b0b8562e8a9a7c33fdc62d0095e9dd2e66b
Ankubito <prudent.nkubito@gmail.com>
2024-06-23 12:54:15 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import sys
-    from sys import argv
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-    if len(argv) != 4:
-        print("Usage: {} username password database".format(argv[0]))
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
         sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    engine = create_engine(
+        f'mysql+mysqldb://{username}: {password}@localhost: 3306/{database}'
+    )
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
 
-    with Session() as session:
-        state_delete = session.query(State).filter(
-                  State.name.like('%a%')).all()
+    states_to_delete = session.query(State).filter(
+        State.name.like('%a%')
+    ).all()
+    for state in states_to_delete:
+        session.delete(state)
 
-        for state in state_delete:
-            session.delete(state)
-
-        session.commit()
+    session.commit()
+    session.close()
