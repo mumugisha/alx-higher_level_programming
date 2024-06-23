1bb97058c65b2591e373c799cf8fc268a03e92c1
Ankubito <prudent.nkubito@gmail.com>
2024-06-23 14:23:26 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -29,5 +29,9 @@ if __name__ == "__main__":
     ).order_by(City.id).all()
 
     for row in cities_name:
-        print("{}: ({}) {}".format(row.State.name[2], row.id[0], row.city.name[1]))
+        print("{}: ({}) {}".format(
+            row.State.name[2],
+            row.id[0],
+            row.city.name[1]
+            ))
     session.close()
