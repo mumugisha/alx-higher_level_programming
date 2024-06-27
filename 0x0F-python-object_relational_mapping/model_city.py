f8761f22e79678791012f03d33750348e85a212f
Ankubito <prudent.nkubito@gmail.com>
2024-06-27 14:29:10 -0700

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+Contains the class definition of a City.
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
+
 
 class City(Base):
     __tablename__ = 'cities'
