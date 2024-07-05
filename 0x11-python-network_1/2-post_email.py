37db294e6de71decaa5de83e1de48dc83f58b5f4
mumugisha <prudent.nkubito@gmail.com>
2024-07-05 15:05:52 -0700

    Python Networking Project

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,9 +12,9 @@ from urllib import parse, request
 if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
-    
+
     data = parse.urlencode({'email': email}).encode('ascii')
     req = request.Request(url, data)
-    
+
     with request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
