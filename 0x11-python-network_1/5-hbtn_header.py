65aa099c3470f204f87e5736b458be5d21a0b18b
mumugisha <prudent.nkubito@gmail.com>
2024-07-05 17:17:15 -0700

    Python Networking Project

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request 
-to the URL and displays the value of the variable 
-X-Request-Id in the response header
+Python script that takes in a URL, sends a request
+to the URL and displays the value of the variable
+X-Request-Id in the response header.
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from requests import get
     from sys import argv
-    
+
     response = get(argv[1])
-    print(response.headers.get('X-Request-Id'))
+    print(response.headers.get("X-Request-Id"))
+
