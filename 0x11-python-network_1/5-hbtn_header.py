4a325f3db81d4b2d7843f0e4b785571e42fdd58b
mumugisha <prudent.nkubito@gmail.com>
2024-07-06 04:39:24 -0700

    Python Networking Project

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,6 +8,7 @@ X-Request-Id in the response header.
 from requests import get
 from sys import argv
 
+
 if __name__ == "__main__":
     response = get(argv[1])
     print(response.headers.get("X-Request-Id"))
