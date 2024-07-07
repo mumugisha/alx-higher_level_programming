db322984115b28981799e4a8161551106f2fd7cc
mumugisha <prudent.nkubito@gmail.com>
2024-07-07 10:45:37 -0700

    Python Networking Project

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == '__main__':
     req = requests.get('https://alx-intranet.hbtn.io/status')
     print(
-        "Body req:\n\t- type: {}\n\t- content: {}".format(
+        "Body response:\n\t- type: {}\n\t- content: {}".format(
             type(req.text), req.text
         )
     )
