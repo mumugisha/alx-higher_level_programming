d06928f0c1e2709516a4a10941c0cd078880d294
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 13:58:11 -0700

    Web Scrapping

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+
+// Script that prints the title of a Star Wars movie
+// where the episode number matches a given integer.
+
+
+const request = require('request');
+const argv = process.argv;
+const requestUrl = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
+
+
+request(requestUrl, function (err, response, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.error(`Error: ${response.statusCode}`);
+  }
+});
