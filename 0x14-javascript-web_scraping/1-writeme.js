277c6739fb83b23d9d94a2452e57b06e3461528d
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 12:35:14 -0700

    Web Scrapping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
 
+
 // Write a script that writes a string to a file
+
 
 const fs = require('fs');
 const filePath = process.argv[2];
 const stringWrite = process.argv[3];
 
+
 // Write files using Js
-fs.writeFile(filePath, stringWrite, 'utf8', (err) => {
+fs.writeFile(filePath, stringWrite, 'utf-8', (err) => {
   if (error) {
-    console.log(error);
+      return console.log(error);
   }
 });
