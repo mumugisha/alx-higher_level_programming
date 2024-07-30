2d7668cdf6b2c7d0c10689feab48f0b64d6b12e0
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 23:40:10 +0300

    Web Scrapping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-
-// Write a script that displays the status code of a GET request
+// Write a script that display the status code of a GET request
 
 
 const request = require('request');
-const argv = process.argv;
+const requestUrl = process.argv[2];
 
 
-request(argv[2], (err, response) => {
-  if (err) {
-    console.error('Error:', err);
-    return;
-  }
-  console.log('code:', response && response.statusCode);
+request(requestUrl, (err, response) => {
+	  if (err) {
+		      console.error('Error:', err);
+		      return;
+		    }
+	  console.log('code:', response.statusCode);
 });
+
