fcdf5eeb597c6f497d4e8906e6b2ff218122e0d4
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 16:34:42 -0700

    Web Scrapping

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+// Write a script that computes the number
+// of tasks completed by user id.
+
+const request = require('request');
+const { argv } = process;
+const api = 'https://swapi-api.alx-tools.com/api';
+
+request(`${api}/films/${argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.error('Error fetching film:', err);
+    return;
+  }
+
+  if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, (err, response, body) => {
+        if (err) {
+          console.error('Error fetching character:', err);
+          return;
+        }
+        if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
