9dbe4a80121755dc729cf380f580881782b848b7
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 16:44:34 -0700

    Web Scrapping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 
-// Write a script that computes the number
-// of tasks completed by user id.
-
 const request = require('request');
 const { argv } = process;
 const api = 'https://swapi-api.alx-tools.com/api';
+
+// Function to fetch and print character names
+const fetchCharacterNames = (characterUrls) => {
+  characterUrls.forEach((character) => {
+    request(character, (err, response, body) => {
+      if (err) {
+        console.error('Error fetching character:', err);
+        return;
+      }
+      if (response.statusCode === 200) {
+        console.log(JSON.parse(body).name);
+      }
+    });
+  });
+};
 
 request(`${api}/films/${argv[2]}`, (err, response, body) => {
   if (err) {
@@ -15,16 +27,6 @@ request(`${api}/films/${argv[2]}`, (err, response, body) => {
 
   if (response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, (err, response, body) => {
-        if (err) {
-          console.error('Error fetching character:', err);
-          return;
-        }
-        if (response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    fetchCharacterNames(characters);
   }
 });
