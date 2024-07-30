bf54bba283a09a69e644819ecbfbb1c74e7eb0ba
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 15:09:40 -0700

    Web Scrapping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 // Write a script that writes a string to a file
-//
-// const fs = require('fs');
-// const filePath = process.argv[2];
-// const contentWrite = process.argv[3];
-//
-// // Write files using Js
-// fs.writeFile(filePath, contentWrite, 'utf-8', (err) => {
-//   if (err) {
-//       console.log(err);
-//         }
-//         });
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const contentWrite = process.argv[3];
+
+// Write files using Js
+fs.writeFile(filePath, contentWrite, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,14 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
-// Write a script that displays the status code of a GET request
-//
-// const request = require('request');
-// const requestUrl = process.argv[2];
-//
-// request(requestUrl, (err, response) => {
-//   if (err) {
-//       console.error('Error:', err);
-//           return;
-//             }
-//               console.log('code:', response && response.statusCode);
-//               });
-//
+// Write a script that display the status code of a GET request
+
+const request = require('request');
+const requestUrl = process.argv[2];
+
+request(requestUrl, (err, response) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+  console.log('code:', response && response.statusCode);
+});
