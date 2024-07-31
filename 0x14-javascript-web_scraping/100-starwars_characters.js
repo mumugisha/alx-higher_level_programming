9820314bdcd1e567bd596a4612a7db1ef588f2f8
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 17:07:47 -0700

    Web Scrapping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,27 +5,28 @@
 
 const request = require('request');
 const { argv } = process;
-const api = 'https://swapi-api.alx-tools.com/api';
+const api = 'https://swapi-api.alx-tools.com/api/films';
 
-request(`${api}/films/${argv[2]}`, (err, response, body) => {
+const fetchCharacterName = (characterUrl) => {
+  request(characterUrl, (err, response, body) => {
+    if (err) {
+      console.error('Error fetching character:', err);
+      return;
+    }
+    if (response.statusCode === 200) {
+      const name = JSON.parse(body).name;
+      console.log(name);
+    }
+  });
+};
+
+request(`${api}/${argv[2]}`, (err, response, body) => {
   if (err) {
     console.error('Error fetching film:', err);
     return;
   }
-
   if (response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, (err, response, body) => {
-        if (err) {
-          console.error('Error fetching character:', err);
-          return;
-        }
-        if (response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    characters.forEach(fetchCharacterName);
   }
 });
-
