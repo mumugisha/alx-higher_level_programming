01f04a9d4f81692e79b7e511f5af81bcbe446678
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 17:48:56 -0700

    Web Scrapping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,34 +1,16 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-// Write a script that prints all characters of a Star Wars movie:
-
-var request = require('request');
-var url = 'https://swapi-api.alx-tools.com/api/films/' + args()[0];
-
-function printCharacters (chars, idx) {
-  request(chars[idx], function (err, res, body) {
+function printCharacters(chars, idx) {
+  request(chars[idx], (err, res, body) => {
     if (!err) {
       console.log(JSON.parse(body).name);
-      if (idx + 1 < chars.length) {
-        printCharacters(chars, idx + 1);
-      }
+      if (idx + 1 < chars.length) printCharacters(chars, idx + 1);
     }
   });
 }
 
-function varName () {
-  return 'value';
-}
-
-function args () {
-  return process.argv.slice(2);
-}
-
-// Call varName to avoid no-unused-vars error
-varName();
-
-request(url, function (err, res, body) {
-  if (!err) {
-    printCharacters(JSON.parse(body).characters, 0);
-  }
+request(url, (err, res, body) => {
+  if (!err) printCharacters(JSON.parse(body).characters, 0);
 });
