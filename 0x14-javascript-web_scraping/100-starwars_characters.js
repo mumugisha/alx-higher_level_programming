0d8dcc7c948e99241ebbcc10bf569887e3c15aa0
mumugisha <prudent.nkubito@gmail.com>
2024-07-30 17:22:57 -0700

    Web Scrapping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,17 +3,26 @@
 // Script that prints all characters of a Star Wars movie
 
 const request = require('request');
-const { argv } = process;
+const argv = process.argv;
 const api = 'https://swapi-api.alx-tools.com/api';
 
-request(`${api}/films/${argv[2]}`, function (_error, response, body) {
-  const characters = [];
+// Check if a movie ID is provided
+if (argv.length < 3) {
+  console.error('Usage: ./script.js <movie_id>');
+  process.exit(1);
+}
+
+const movieId = argv[2];
+
+request(api + '/films/' + movieId, function (_error, response, body) {
   if (response.statusCode === 200) {
-    characters.push(...JSON.parse(body).characters);
-    for (const character of characters) {
-      request(character, function (_error, _response, body) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach(function (characterUrl) {
+      request(characterUrl, function (_error, _response, body) {
         console.log(JSON.parse(body).name);
       });
-    }
+    });
+  } else {
+    console.error('Failed to retrieve movie data.');
   }
 });
